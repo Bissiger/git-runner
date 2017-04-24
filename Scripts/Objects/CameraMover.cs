927fbfac6d1924925ccfe07a170aef41158ef47f
Bissiger <savchenko-a@mail.ru>
2017-04-24 16:10:28 +0300

    Add some distance from camera to object

--- Scripts/Objects/CameraMover.cs
+++ Scripts/Objects/CameraMover.cs
@@ -4,10 +4,11 @@ namespace CVRunner
 {
     public class CameraMover : BaseObjectScene
     {
-        
-        [SerializeField] private float _speed = 0.3f;    //Speed of camera mooving
-        [SerializeField] private GameObject targetPos;     //Camera will be moved to this target
-        private bool isActive = false;                  //flag ON/OFF moving
+
+        [SerializeField] private float _speed = 0.3f;       //Speed of camera mooving
+        [SerializeField] private GameObject targetPos;      //Camera will be moved to this target
+        [SerializeField] private float _distToObj = 2.5f;   //Distance to object from Camera   
+        private bool isActive = false;                      //flag ON/OFF moving
 
         private void Update()
         {
@@ -35,7 +36,7 @@ namespace CVRunner
             {
                 float x = Mathf.Lerp(Position.x, tmpTargetPos.x, _speed * Time.deltaTime);
                 float y = Mathf.Lerp(Position.y, tmpTargetPos.y, _speed * Time.deltaTime);
-                float z = Mathf.Lerp(Position.z, tmpTargetPos.z, _speed * Time.deltaTime);
+                float z = Mathf.Lerp(Position.z, tmpTargetPos.z-_distToObj, _speed * Time.deltaTime);
                 Position = new Vector3 (x,y,z);
             }
         }
