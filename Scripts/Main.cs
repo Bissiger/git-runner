8bc34d7bb0e6c7175820754a5a567f7994891b05
Bissiger <savchenko-a@mail.ru>
2017-04-26 16:21:15 +0300

    At start scene creating PlayerCar object and get link on this object

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -8,11 +8,14 @@ namespace CVRunner
     /// </summary>
     public sealed class Main : MonoBehaviour
     {
+        [SerializeField] GameObject Player;
+
         public static Main Instance { get; private set; }
 
         private GameObject _controllers;
         private DirectionController _directionController;
         private CameraMover _cameraMover;
+        private GameObject _player;
 
         private void Start()
         {
@@ -20,22 +23,30 @@ namespace CVRunner
             _controllers = new GameObject { name = "AllControllers" };
             _directionController = _controllers.AddComponent<DirectionController>();
             _cameraMover = GameObject.Find("Main Camera").transform.GetComponent<CameraMover>();
+            _player = Instantiate(Player);
         }
 
         #region Property
         /// <summary>
-        /// Get GetCameraMover controller
+        /// Get GetCameraMover link
         /// </summary>
         public CameraMover GetCameraMover
         {
             get { return _cameraMover; }
         }
         /// <summary>
-        /// Get DirectionController controller
+        /// Get DirectionController link
         /// </summary>
         public DirectionController GetDirectionController
         {
             get { return _directionController; }
+        }
+        /// <summary>
+        /// Get Player link
+        /// </summary>
+        public GameObject GetPlayer
+        { 
+            get { return _player; }
         }
         #endregion
     }
