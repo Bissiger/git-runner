336ef95b661cdd1b71106f7ef463fc99b674670a
Bissiger <savchenko-a@mail.ru>
2017-05-24 13:25:56 +0300

    Start working on SityCreator and rules of moving

---
+++ Scripts/Objects/SityMover.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace CVRunner
+{
+    public class SityMover : BaseObjectScene
+    {
+        private float speed = 0.5f;
+
+        private void Update()
+        {
+            if (!Main.Instance.GetPause.IsPaused)
+            {
+                Move();
+            }
+        }
+
+        private void Move()
+        {
+            Position = new Vector3(Position.x, Position.y, Position.z - speed);
+        }
+    }
+}
