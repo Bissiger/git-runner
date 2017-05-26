4293d8748a27bc10e591b9bcfa79b7c0de199da2
Bissiger <savchenko-a@mail.ru>
2017-05-26 12:10:48 +0300

    Data script. Its will be save data in future

---
+++ Scripts/Helpers/Data.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data : MonoBehaviour {
+
+    //Speed of CityMoving
+    [SerializeField] private float _speed = 0.5f;
+
+    public float Speed
+    {
+        get { return _speed; }
+        set { _speed = value; }
+    }
+}
