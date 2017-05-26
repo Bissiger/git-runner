5aebd50385edd9c8f646c3f9a1e03a9d06d730fd
Bissiger <savchenko-a@mail.ru>
2017-05-26 12:08:31 +0300

    Just renamed =)

---
+++ Scripts/Objects/CityCreator.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+namespace CVRunner
+{
+    public class CityCreator : BaseObjectScene
+    {
+        //Road prebaf
+        [SerializeField] private GameObject road;
+        //Position where creating gameObject
+        [SerializeField] private Vector3 creatingPos;
+        //Life time of segment
+        [SerializeField] private float lifeTime = 8f;
+        //Step for next position of road segment creating
+        private float step = 120f;
+        //Speed deviation
+        private float deviation = 0f;
+        
+        private GameObject tmpGameObj;
+
+        private void Start()
+        {
+            creatingPos = new Vector3(-8.5f, 0f, -3f);
+            Create();
+        }
+
+
+        private void Update()
+        {
+            if (!Main.Instance.GetPause.IsPaused)
+            {
+                Create();
+            }
+        }
+
+        private void Create()
+        {
+            //work once
+            if (!tmpGameObj)
+            {
+                deviation = Main.Instance.GetData.Speed;
+                tmpGameObj = Instantiate(road, creatingPos, Quaternion.identity);
+                tmpGameObj.AddComponent<CityMover>();
+                Destroy(tmpGameObj, lifeTime);
+            }
+            else if (tmpGameObj.transform.position.z < -3f)
+            {
+                deviation = Main.Instance.GetData.Speed;
+                step -= deviation;
+                creatingPos = new Vector3(-8.5f, 0f, tmpGameObj.transform.position.z + step);
+                tmpGameObj = Instantiate(road, creatingPos, Quaternion.identity);
+                tmpGameObj.AddComponent<CityMover>();
+                Destroy(tmpGameObj, lifeTime);
+            }
+        }
+    }
+}
---
+++ Scripts/Objects/CityMover.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace CVRunner
+{
+    public class CityMover : BaseObjectScene
+    {
+        private float speed;
+
+        private void Update()
+        {
+            if (!Main.Instance.GetPause.IsPaused)
+            {
+                speed = Main.Instance.GetData.Speed;
+                Position = new Vector3(Position.x, Position.y, Position.z - speed);
+            }
+        }
+    }
+}
