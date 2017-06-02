1e0a725de9ed1f137a787f991d8801b2fe98dd4c
Bissiger <savchenko-a@mail.ru>
2017-06-02 13:08:24 +0300

    Change names

---
+++ Scripts/Objects/CityCreator.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace CVRunner
+{
+    public class CityCreator : BaseObjectScene
+    {
+        //Road prebaf
+        [SerializeField] GameObject road;
+        //Position where creating gameObject
+        [SerializeField] private Vector3 creatingPos;
+
+        //Deviation of road position
+        private float deviation = -32f;
+
+        //Life time o road element
+        private float lifeTime = 8f;
+
+        private GameObject tmpGameObj;
+
+        private void Start()
+        {
+            creatingPos = new Vector3(-8.5f, 0f, -16f);
+            Create();
+        }
+
+
+        private void Update()
+        {
+            if (!Main.Instance.GetPause.IsPaused)
+            {
+                Create();
+            }
+        }
+
+        private void Create()
+        {
+            if (!tmpGameObj)
+            {
+                tmpGameObj = Instantiate(road, creatingPos, Quaternion.identity);
+                tmpGameObj.AddComponent<CityMover>();
+            }
+            else if (tmpGameObj.transform.position.z < deviation)
+            {
+                float z = tmpGameObj.transform.position.z + 119f; 
+                creatingPos = new Vector3(-8.5f, 0f, z);
+                tmpGameObj = Instantiate(road, creatingPos, Quaternion.identity);
+                tmpGameObj.AddComponent<CityMover>();
+            }
+        }
+    }
+}
---
+++ Scripts/Objects/CityMover.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace CVRunner
+{
+    public class CityMover : BaseObjectScene
+    {
+        //Time of life an object
+        private float lifeTime = 8f;
+
+        private void Update()
+        {
+            if (!Main.Instance.GetPause.IsPaused)
+            {
+                Move();
+            }
+        }
+
+        private void Move()
+        {
+            float speed = Main.Instance.GetData.Speed;
+            Position = new Vector3(Position.x, Position.y, Position.z - speed);
+            Destroy(gameObject, lifeTime);         
+        }
+    }
+}
